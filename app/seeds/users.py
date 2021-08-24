d3d8b3585a6c7ab324c8a9e21c6afeb7c429fcae
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-24 17:54:08 -0600

    working on backend

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,12 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='Demo', email='demo@aa.io', first_name='Demo', last_name='User', password='password')
+    sunny = User(
+        username='sunny', email='sunny@user.com', first_name='Sunny', last_name='Mallick', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(sunny)
 
     db.session.commit()
 
