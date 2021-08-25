3fcc022638bfebde2522d9888b2ccf7bdac699a2
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-25 17:51:36 -0600

    added backend route info for auction component

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from flask.helpers import url_for
+from app.models import auction
 import os
 from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
@@ -8,6 +10,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.auction_routes import auction_routes
 
 from .seeds import seed_commands
 
@@ -31,6 +34,7 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(auction_routes, url_prefix='/api/auctions')
 db.init_app(app)
 Migrate(app, db)
 
