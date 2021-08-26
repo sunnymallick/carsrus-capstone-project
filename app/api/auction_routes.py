eef94dd6a7f68e74afbe413ae6146b709d20c16a
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-26 09:57:22 -0600

    fixed login issues, working on auction route

--- app/api/auction_routes.py
+++ app/api/auction_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import Auction
+from app.forms.auction_form import AuctionForm
 
 auction_routes = Blueprint('auctions', __name__)
 
@@ -10,7 +11,19 @@ def auctions():
     return {'auctions': [auction.to_dict() for auction in auctions]}
 
 @auction_routes.route('/<int:id>')
-def auctions(id):
+def get_auctions(id):
     auction = Auction.query.get(id)
     return auction.to_dict()
+
+@auction_routes.route('/form')
+def auction_form():
+    form = AuctionForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        auction = Auction(
+            vin=form.data['vin'],
+            year=form.data['year'],
+            make=form.data['make'],
+            model=form.data['model']
+        )
 
