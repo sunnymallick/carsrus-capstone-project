88fc7b5c653e6b5079471f6aaad407b2e8947474
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-26 21:47:30 -0600

    working on profile by id

--- react-app/src/components/UserProfile/User.js
+++ react-app/src/components/UserProfile/User.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';
+import { getAuctions } from '../../store/auction';
 
 function User() {
   const [user, setUser] = useState({});
   const { userId }  = useParams();
+  const sessionUser = useSelector(state => state.session.user)
+  // const userAuctions = useState((state) => state.auction)
 
   useEffect(() => {
     if (!userId) {
       return;
     }
-    (async () => {
-      const response = await fetch(`/api/users/${userId}`);
-      const user = await response.json();
-      setUser(user);
-    })();
   }, [userId]);
 
   if (!user) {
@@ -26,10 +25,10 @@ function User() {
         <strong>User Id</strong> {userId}
       </li>
       <li>
-        <strong>Username</strong> {user.username}
+        <strong>Username</strong> {sessionUser.username}
       </li>
       <li>
-        <strong>Email</strong> {user.email}
+        <strong>Email</strong> {sessionUser.email}
       </li>
     </ul>
   );
