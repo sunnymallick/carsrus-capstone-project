608e2732da09bd65cf0cf309d91c003f58049dbb
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-26 22:41:15 -0600

    added date comparison for auction

--- react-app/src/components/UserProfile/User.js
+++ react-app/src/components/UserProfile/User.js
@@ -8,28 +8,43 @@ function User() {
   const sessionUser = useSelector(state => state.session.user)
   const auctions = useSelector(state => Object.values(state.auction))
   const userAuctions = auctions.filter(auction => auction?.user_id === +userId)
-  console.log(userAuctions)
+  // console.log(userAuctions)
   // console.log(userAuctions)
   const dispatch = useDispatch();
+  const currentDate = new Date()
 
   useEffect(async () => {
     if (!userId) {
       return;
     }
     await dispatch(getAuctions())
-  }, [userId]);
+  }, [userId, dispatch]);
+
 
   return (
     <>
       <h3>{sessionUser.first_name} {sessionUser.last_name}</h3>
         <div className='user-auction-container'>
-        <h3>Your listings</h3>
+        
         {userAuctions.map(auction => {
+          if (new Date(auction.end_date) < new Date(currentDate)) {
+            return (
+              <>
+                <h3>Past Listings</h3>
+                <div className='user-auction-listing-past'>
+                  <h3>{auction.year} {auction.make} {auction.model}</h3>
+                  <h3>Auction ended on {new Date(auction.end_date).toLocaleDateString()}</h3>
+                </div>
+              </>
+            )
+          }
           if (auction?.id) {
             return (
               <>
-                <div className='user-auction-listing'>
+                <div className='user-auction-listing-present'>
+                  <h3>Your Current Listings</h3>
                   <h3>{auction.year} {auction.make} {auction.model}</h3>
+                  <h3>Auction ends on {new Date(auction.end_date).toLocaleDateString()}</h3>
                 </div>
               </>
             )
