1f699a61cf99cc08a3693d17e1bba72a9af0c71c
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-30 23:59:16 -0600

    added images backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask.helpers import url_for
-from app.models import auction
+from app.models import auction, image
 import os
 from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
@@ -13,6 +13,7 @@ from .api.auth_routes import auth_routes
 from .api.auction_routes import auction_routes
 from .api.bid_routes import bid_routes
 from .api.comment_routes import comment_routes
+from .api.image_routes import image_routes
 
 from .seeds import seed_commands
 
@@ -39,6 +40,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(auction_routes, url_prefix='/api/auctions')
 app.register_blueprint(bid_routes, url_prefix='/api/bids')
 app.register_blueprint(comment_routes, url_prefix='/api/comments')
+app.register_blueprint(image_routes, url_prefix='/api/images')
 db.init_app(app)
 Migrate(app, db)
 
