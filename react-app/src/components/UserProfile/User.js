5654c005aeee1791f39d1bd0b14b379ed5227329
Sunny Mallick <sunny.mallick927@gmail.com>
2021-08-30 22:57:54 -0600

    fixed issue with all vehicles not showing up on profile

--- react-app/src/components/UserProfile/User.js
+++ react-app/src/components/UserProfile/User.js
@@ -9,10 +9,12 @@ import EditAuctionModal from '../EditAuctionModal';
 function User() {
   const { userId }  = useParams();
   const sessionUser = useSelector(state => state.session.user)
+  const currentDate = new Date()
   const auctions = useSelector(state => Object.values(state.auction))
   const userAuctions = auctions.filter(auction => auction?.user_id === +userId)
+  const pastAuctions = userAuctions.filter(auction => new Date(auction.end_date) > new Date(currentDate))
+  const currentAuctions = userAuctions.filter(auction => new Date(auction.end_date) < new Date(currentDate))
   const dispatch = useDispatch();
-  const currentDate = new Date()
 
   useEffect(async () => {
     if (!userId) {
@@ -21,45 +23,31 @@ function User() {
     await dispatch(getAuctions())
   }, [userId, dispatch]);
 
-  let pastAuctions;
-  let currentAuctions;
-  if (sessionUser) {
-    userAuctions.map(auction => {
-      if (new Date(auction.end_date) < new Date(currentDate)) {
-        pastAuctions = (
-            <div className='user-auction-listing-past'>
-              <h3>{auction.year} {auction.make} {auction.model}</h3>
-              <h3>Auction ended on {new Date(auction.end_date).toLocaleDateString()}</h3>
-            </div>
-        )
-      
-    } else if (new Date(auction.end_date) > new Date(currentDate)) {
-      currentAuctions = (
-          <div className='user-auction-listing-present'>
-            <h3>{auction.year} {auction.make} {auction.model}</h3>
-            <div className='edit-delete-buttons'>
-              <EditAuctionModal auctionId={auction?.id} />
-              <DeleteAuctionModal auctionId={auction?.id} />
-            </div>
-            <h3>Auction ends on {new Date(auction.end_date).toLocaleDateString()}</h3>
-          </div>
-      )
-    }
-  })
-
-  }
-
   return (
     <>
       <h3>{sessionUser.first_name} {sessionUser.last_name}</h3>
         <div className='user-auction-container'>
           <div className='current-listings-container'>
-            <h3>Current Listings</h3>
-              {currentAuctions}
+            <h2>Current Listings</h2>
+            {currentAuctions.map(auction => {
+              return (
+                <>
+                <h3>{auction.year} {auction.make} {auction.model}</h3>
+                <div className='edit-delete-buttons'>
+                  <EditAuctionModal auctionId={auction?.id} />
+                  <DeleteAuctionModal auctionId={auction?.id} />
+                </div>
+                </>
+              )
+            })}
           </div>
           <div className='past-listings-container'>
-            <h3>Past Auctions</h3>
-            {pastAuctions}
+            <h2>Past Auctions</h2>
+            {pastAuctions.map(auction => {
+              return (
+                <h3>{auction.year} {auction.make} {auction.model}</h3>
+              )
+            })}
           </div>
         </div>
     </>
